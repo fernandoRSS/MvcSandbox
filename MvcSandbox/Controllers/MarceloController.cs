69d1f672bb4a24f22e27e18f9e694f79234c0a33
Fernando Ribeiro Salvador de Sa <ribeiro.rs@gmail.com>
2018-08-10 14:16:33 -0300

    Add project files.

---
+++ MvcSandbox/Controllers/MarceloController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcSandbox.Controllers
+{
+    public class MarceloController : Controller
+    {
+        // GET: Marcelo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Marcelo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Marcelo/Create
+        [Route("Maaa/SaveLocation")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Marcelo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Marcelo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Marcelo/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Marcelo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Marcelo/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MvcSandbox/JSON/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcSandbox.JSON
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public bool Sexo { get; set; }
+    }
+}
